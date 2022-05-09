0c08ff4757805c6d345e72892426775d77bbfe5f
acontrerasregueiro <acontrerasregueiro@gmail.com>
2022-05-09 19:20:16 +0000

    ejemplo Bootstrap Landing Page REACT

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<div className="mt-2 p-1 bg-secondary text-white rounded">
+			<h1>Footer Example</h1>
+			<p>Lorem ipsum...</p>
+		</div>
+	);
+};
+
+export default Footer;
